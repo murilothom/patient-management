f7d6a63a54ff503e225ae22b862b1b7da2cb8ffb
Murilo Thom <murilothom.2002@hotmail.com>
2024-01-22 15:14:08 -0300

    fix: remove non used imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
 import { Home } from './pages/Home';
-import 'react-toastify/dist/ReactToastify.css';
 import { ModalProvider } from './contexts/ModalContext';
 import { PatientsProvider } from './contexts/PatientsContext';
 import { Toaster } from 'react-hot-toast';
