8df2cb947b3fabc826dded3584e18dc5bbeb24a9
Murilo Thom <murilothom.2002@hotmail.com>
2024-01-23 07:07:07 -0300

    fix: patient api url

--- src/services/patients-service.ts
+++ src/services/patients-service.ts
@@ -3,8 +3,8 @@ import { PatientSchema } from '../lib/formik/patient/validation-schema';
 import { Patient } from '../types/patient';
 import { Service } from './service';
 
-// const baseURL = 'https://patient-management-api-5mh6.onrender.com/patient'; /* URL Caso API NÃO estiver sendo executada localmente */
-const baseURL = 'http://localhost:3000/patient'; /* URL Caso API estiver sendo executada localmente */
+const baseURL = 'https://patient-management-api-5mh6.onrender.com/patient'; /* URL Caso API NÃO estiver sendo executada localmente */
+// const baseURL = 'http://localhost:3000/patient'; /* URL Caso API estiver sendo executada localmente */
 
 export class PatientsService {
   constructor(private service: Service) {}
